434eea84055bcdd1cac4e2cba8bc799941d597e6
Mark Hustad <hustada80@gmail.com>
2018-03-13 23:46:55 -0500

    add reducers directory and auth code from firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers  from './reducers';
+import firebase from 'firebase';
 
 
 class App extends Component {
+	componentWillMount() {
+			const config = {
+	    apiKey: 'AIzaSyBDqdv2SV-LR4YbgbC1xxYHPBvBCm_xDn4',
+	    authDomain: 'manager-40e63.firebaseapp.com',
+	    databaseURL: 'https://manager-40e63.firebaseio.com',
+	    projectId: 'manager-40e63',
+	    storageBucket: 'manager-40e63.appspot.com',
+	    messagingSenderId: '860908591557'
+	  };
+  	firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
-			<Provider>
+			<Provider store={createStore(reducers)}>
 				<View>
 					<Text>
 						Hello!
