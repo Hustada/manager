e985c3a1679bd5d46a9e759b32699c11c2a1c619
Mark Hustad <mark.hustad@firespring.com>
2018-03-14 15:07:19 -0500

    s

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,8 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase from 'firebase';
+import { EMAIL_CHANGED,
+		 PASSWORD_CHANGED 
+} from './types';
+
 
 export const emailChanged = (text) => {
 	return {
@@ -12,4 +16,8 @@ export const passwordChanged = (text) => {
 		type: PASSWORD_CHANGED,
 		payload: text
 	};
+};
+
+export const loginUser = ({ email, password }) => {
+	firebase.auth.signInWithEmailAndPassword(email, password).then(user => console.log(user));
 };
