dcc7f85b9d38a905e1cbfed0ec0efae798fb9bed
Mark Hustad <hustada80@gmail.com>
2018-03-17 10:50:29 -0500

    add picker for days of week

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,10 +3,17 @@ import {
 } from '../actions/types';
 
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+	name: '',
+	phone: '',
+	shift: ''
+};
 
-export default ( state = INITIAL_STATE, action)m => {
+export default ( state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case EMPLOYEE_UPDATE:
+			// action.payload === { prop: 'name', value: 'jane' }
+			return { ...state,  [action.payload.prop]: action.payload.value }
 		default:
 			return state;
 	}
